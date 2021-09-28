820a0a8f6563424d7653c2313922ce5549a2a345
embtraining <90279264+embtraining@users.noreply.github.com>
2021-09-28 21:08:56 +0530

    Update stdlib.h

--- Preprocessor/stdlib.h
+++ Preprocessor/stdlib.h
@@ -13,3 +13,15 @@ abort()
 exit(0)
 atexit()
 system()
+
+  
+  
+  About Stdlib
+
+abort():Terminate program abnormally abort
+
+exit(0):Terminates normally.what all the files open your dir closes/flushes it.In normally peacefully terminated.
+
+atexit():After execution of current program you want to execute some function in that situation at exit will be used.
+
+system():Able to run system terminal command 
